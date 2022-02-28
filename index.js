80d8e6a740758d897c80d6f3dfbb7ab9fee4b512
Matthew Stead <mattstead6@gmail.com>
2022-02-27 21:43:44 -0500

    commit tree 8001c805d62638e5c7a5a114b0d3de5c47d2eaca

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// const totalBatteries = function batteries(arr, reducer){
+//     batteryBatches
+// }
+
+// function reducer(batteryBatches){
+//     let total = 0
+//     return batteryBatches+=total
+// }
+
+
+const totalBatteries = batteryBatches.reduce((aggregate, number) => aggregate + number);
